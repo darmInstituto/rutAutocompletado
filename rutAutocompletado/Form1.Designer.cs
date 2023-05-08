7f172356e41c429a24275fa602e03bd33c1f0465
Darm02 <david02rangel@gmail.com>
2023-05-07 20:59:39 -0400

    Agregar archivos de proyecto.

---
+++ rutAutocompletado/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace rutAutocompletado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRut
+            // 
+            this.txtRut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRut.Location = new System.Drawing.Point(111, 84);
+            this.txtRut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.ShortcutsEnabled = false;
+            this.txtRut.Size = new System.Drawing.Size(265, 30);
+            this.txtRut.TabIndex = 0;
+            this.txtRut.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRut_KeyPress);
+            this.txtRut.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtRut_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(187, 147);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 341);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtRut);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ rutAutocompletado/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rutAutocompletado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<char> listRut = new List<char>();
+        private void txtRut_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int seleccion = txtRut.SelectionStart;
+
+            if (!Char.IsControl(e.KeyChar))
+            {
+                if (Char.IsDigit(e.KeyChar) || e.KeyChar == 'k' || e.KeyChar == 'K')
+                {
+                    int longitud = listRut.Count;
+                    e.Handled = true;
+
+                    if (longitud >= 12)
+                    {
+
+                    }
+                    else if (txtRut.TextLength > 0)
+                    {
+                        listRut.Clear();
+                        for (int i = 0; i < txtRut.Text.Length; i++)
+                        {
+                            listRut.Add(txtRut.Text[i]);
+                        }
+
+                        //MessageBox.Show(seleccion.ToString());
+
+                        listRut.Insert(seleccion, e.KeyChar);
+                        seleccion++;
+
+                        while (listRut.IndexOf('-') >= 0)
+                        {
+                            seleccion--;
+                            listRut.Remove('-');
+                        }
+                        while (listRut.IndexOf('.') >= 0)
+                        {
+                            seleccion--;
+                            listRut.Remove('.');
+                        }
+
+                        if (listRut.Count > 1)
+                        {
+                            seleccion++;
+                            listRut.Insert(listRut.Count - 1, '-');
+                        }
+
+                        if (listRut.Count > 5)
+                        {
+                            seleccion++;
+                            listRut.Insert(listRut.Count - 5, '.');
+                        }
+
+                        if (listRut.Count > 9)
+                        {
+                            seleccion++;
+                            listRut.Insert(listRut.Count - 9, '.');
+                        }
+
+                        txtRut.Text = String.Join("", listRut.ToArray());
+                        txtRut.SelectionStart = seleccion;
+                    }
+                    else
+                    {
+                        listRut.Insert(seleccion, e.KeyChar);
+                        txtRut.Text = String.Join("", listRut.ToArray());
+                        txtRut.SelectionStart = txtRut.TextLength;
+                    }
+
+
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+
+            }
+
+        }
+
+        private void txtRut_KeyUp(object sender, KeyEventArgs e)
+        {
+            int seleccion = txtRut.SelectionStart;
+
+            if (e.KeyValue == 8 || e.KeyValue == 46)
+            {
+                listRut.Clear();
+                if (txtRut.TextLength > 0)
+                {
+                    for (int i = 0; i < txtRut.Text.Length; i++)
+                    {
+                        if (Char.IsNumber(txtRut.Text[i]) || txtRut.Text[i] == 'k' || txtRut.Text[i] == 'K')
+                        {
+                            listRut.Add(txtRut.Text[i]);
+                        }
+                        else
+                        {
+                            seleccion--;
+                        }
+                    }
+
+                    if (listRut.Count > 1)
+                    {
+                        listRut.Insert(listRut.Count - 1, '-');
+                        seleccion++;
+                    }
+
+                    if (listRut.Count > 5)
+                    {
+                        listRut.Insert(listRut.Count - 5, '.');
+                        seleccion++;
+                    }
+
+                    if (listRut.Count > 9)
+                    {
+                        listRut.Insert(listRut.Count - 9, '.');
+                        seleccion++;
+                    }
+
+                    txtRut.Text = String.Join("", listRut.ToArray());
+                    txtRut.SelectionStart = seleccion;
+
+                }
+            }
+        }
+
+        bool validarRut(string rut)
+        {
+            bool valido = false;
+            int[] constantes = { 3, 2, 7, 6, 5, 4, 3, 2 };
+
+            while (rut.Length < 10)
+            {
+                rut = "0" + rut;
+            }
+
+            double suma = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                int valorActual = int.Parse(rut[i].ToString());
+                suma += constantes[i] * valorActual;
+            }
+            double division = suma / 11.0;
+            double decimales = division - (int)division;
+            double digitoNumerico = Math.Round(11 - (11 * decimales));
+            char digito;
+            if (digitoNumerico == 11)
+            {
+                digito = '0';
+            }
+            else if (digitoNumerico == 10)
+            {
+                digito = 'k';
+            }
+            else
+            {
+                digito = Convert.ToChar(digitoNumerico.ToString());
+            }
+        
+
+            if (digito == rut[9])
+            {
+                valido = true;
+            }
+            else if (digito == 'k' && rut[9] == 'K')
+            {
+                valido = true;
+            }
+            else
+            {
+                valido = false;
+            }
+            return valido;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtRut.Text.Contains('-'))
+            {
+                string rut = "";
+                foreach (char c in txtRut.Text)
+                {
+                    if (c != '.')
+                    {
+                        rut += c;
+                    }
+                }
+
+                bool correcto = validarRut(rut);
+                label1.Text = correcto ? "Rut correcto" : "Rut invalido";
+            }
+        }
+    }
+}
